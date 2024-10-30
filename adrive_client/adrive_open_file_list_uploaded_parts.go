e66fab71c36b39b71b6ab42847d5e91b55ef107c
archyese <137976693+archyese@users.noreply.github.com>
2024-10-30 20:16:57 +0800

    Update adrive_open_file_list_uploaded_parts.go

--- adrive_client/adrive_open_file_list_uploaded_parts.go
+++ adrive_client/adrive_open_file_list_uploaded_parts.go
@@ -7,6 +7,6 @@ import (
 )
 
 func (c *AdriveClient) AdriveOpenFileListUploadedParts(username string, params *protos.AdriveOpenFileListUploadedPartsParams) (*protos.AdriveOpenFileListUploadedParts, error) {
-	apiUrl := fmt.Sprintf("%s/adrive/v1.0/openFile/list", c.ApiHost)
+	apiUrl := fmt.Sprintf("%s/adrive/v1.0/openFile/listUploadedParts", c.ApiHost)
 	return common.DoJsonRequest[protos.AdriveOpenFileListUploadedParts](username, c.AccessTokenLoader, c.Agent, apiUrl, params)
 }
